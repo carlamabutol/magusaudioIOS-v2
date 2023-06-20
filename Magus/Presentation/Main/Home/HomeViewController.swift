25206134f1b14e03925e6a45e33d2ddfdda8a72a
punzerkunst <jom.cold@gmail.com>
2023-06-20 20:53:23 +0800

    clean up

--- Magus/Presentation/Main/Home/HomeViewController.swift
+++ Magus/Presentation/Main/Home/HomeViewController.swift
@@ -6,9 +6,31 @@
 //
 
 import UIKit
+import RxDataSources
 
 class HomeViewController: CommonViewController {
     
     private let viewModel = HomeViewModel()
     
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var searchBar: UISearchBar!
+    
+    private func setupDataSource() {
+        let dataSource = RxCollectionViewSectionedReloadDataSource<SectionModel<String, Int>> { sectionModel, collectionView, indexPath, item in
+            
+        }
+    }
+}
+
+struct SectionViewModel {
+    var header: String!
+    var items: [String]
+}
+
+extension SectionViewModel: SectionModelType {
+    typealias Item = String
+    init(original: SectionViewModel, items: [String]) {
+        self = original
+        self.items = items
+    }
 }
